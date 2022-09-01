607375f2a3eae91b9a5b8f6ffd8d92cb4d48e7ab
G-vans <jevans.otieno@student.moringaschool.com>
2022-09-01 22:36:08 +0300

    link apiwith the webpage

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,39 @@
-        // Get the modal
-        var modal = document.getElementById('id01');
-        
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//fetch data
+let url = "http://localhost:3000/Sheet1"
+
+fetch(url)
+.then(res => res.json())
+    .then(function (products) {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+        for (let product of products) {
+            out += `
+         <tr>
+            <td>${product.Name}</td>
+            <td>${product.Location}</td>
+            <td>${product.Type}</td>
+            <td>${product.Size}</td>
+            <td>${product.Land}</td>
+            <td>${product.Price}</td>
+            <td>${product.Size}</td>
+            <td>${product.Rent}</td>
+            <td>${product.SC}</td>
+            <td>${product.NetRent}</td>
+            <td>${product.Yield}</td>
+
+      `;
         }
+
+        placeholder.innerHTML = out;
+    });
+   
